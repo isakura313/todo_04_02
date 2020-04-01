a67bd998445dfa0b91aaff28146aa2d3d0e572cb
Yury Sergeyev <60914627+Yorksergeyev@users.noreply.github.com>
2020-04-01 18:38:01 +0300

    Add files via upload

--- code.js
+++ code.js
@@ -2,6 +2,10 @@ import ItemDeal from "./ItemDeal";
 
 import {motiv_arr, ImportantArr, backgroundColor, MonthArray } from "./data.js"
 
+import anime from 'animejs/lib/anime.es.js';
+
+
+
 const add_button = document.querySelector(".button-plus")
 const input = document.querySelector("input")
 const root = document.getElementById("root")
@@ -20,7 +24,14 @@ function changePhrase(){
     document.querySelector("q").innerHTML = motiv_arr[getRand(motiv_arr)];
     // добавить сда анимации, с которой эта цитата появляется
 }
-
+anime({
+    targets: '.motiv',
+    translateX: anime.stagger(10, {grid: [14, 5], from: 'center', axis: 'x'}),
+    translateY: anime.stagger(10, {grid: [14, 5], from: 'center', axis: 'y'}),
+    rotateZ: anime.stagger([0, 90], {grid: [14, 5], from: 'center', axis: 'x'}),
+    delay: anime.stagger(200, {grid: [14, 5], from: 'center'}),
+    easing: 'easeInOutQuad'
+  });
 setInterval(changePhrase, 2000);
 
 
@@ -48,7 +59,7 @@ function createItem(){
 function DrawItem(item){
        root.insertAdjacentHTML('afterBegin', `<div class="wrap-task field is-grouped" id='${+item.createAt}'>
     <button class="has-text-white button is-medium is-fullwidth ${backgroundColor[item.color]}">  ${item.text}
-    <span>${item.createAt.getDate()} ${MonthArray[item.createAt.getMonth()]} </span>
+    <span style='margin-left: 5vh;'>${item.createAt.getDate()} ${MonthArray[item.createAt.getMonth()]} </span>
 
     </button>
     <button class="btn-delete button is-danger is-medium is-outlined">
@@ -58,6 +69,12 @@ function DrawItem(item){
         </span>
     </button>
 </div>`);
+  anime({
+    targets: '.wrap-task',
+    translateY: 30,
+    scale: 1,
+    rotate: '1turn'
+  });
 }
 
 
@@ -93,14 +110,17 @@ input.addEventListener("keypress", (e)=>{
 root.addEventListener("click", (e)=>{
     if(e.target.className == "fa fa-times"){
         var btn = e.target;
-
+        
     }
     let deal = btn.parentNode.parentNode.parentNode;
+    anime({
+        targets: (deal),
+        translateY: 1000
+    });
     setTimeout(()=>{
-
+        deal.remove();
         localStorage.removeItem(deal.id)
         
-        deal.remove();
     }, 300)
 })
 
@@ -132,8 +152,61 @@ select.onchange = () =>{
     ChangeColorEl(select);
 }
 
+document.querySelector('.start').onclick = function (){
+    function del(){
+        let elem = document.querySelector(".del");
+       elem.remove();
+    }setInterval(del, 10000);
+        root.insertAdjacentHTML('afterBegin', `<div class="del" style="display: grid; grid-template-columns: 200px 200px 200px; margin: 30px;">
+        <button class="is-dark " style="width:200px">JS 100+30 = :</button>
+        <input class="is-dark clown" style="width:50px"></input>
+        
 
+ 
+        </div>`)
 
+        anime({
+            targets: '.clown',
+            value: [0, 10030],
+            round: 1,
+            easing: 'easeInOutExpo'
+          });
+
+          anime({
+            targets: '.is-dark',
+            delay: anime.stagger(200, {grid: [14, 5], from: 'center'}),
+            
+            translateX: [
+              { value: 250, duration: 1000, delay: 500 },
+              { value: 0, duration: 1000, delay: 500 }
+            ],
+            translateY: 
+            [
+              { value: -40, duration: 500 },
+              { value: 40, duration: 500, delay: 1000 },
+              { value: 0, duration: 500, delay: 1000 }
+            ],
+            scaleX: [
+              { value: 4, duration: 100, delay: 500, easing: 'easeOutExpo' },
+              { value: 1, duration: 900 },
+              { value: 4, duration: 100, delay: 500, easing: 'easeOutExpo' },
+              { value: 1, duration: 900 }
+            ],
+            scaleY: [
+              { value: [1.75, 1], duration: 500 },
+              { value: 2, duration: 50, delay: 1000, easing: 'easeOutExpo' },
+              { value: 1, duration: 450 },
+              { value: 1.75, duration: 50, delay: 1000, easing: 'easeOutExpo' },
+              { value: 1, duration: 450 }
+            ],
+            easing: 'easeOutElastic(1, .8)',
+            loop: true
+          });
+          
+        }
+        
+
+        
 
 
 
---
+++ data.js
@@ -0,0 +1,19 @@
+const motiv_arr = ["все победы начинаются с победы над самим собой", "just do it", "все победы начинаются с webpack", "делу время, потехе час"]
+
+
+const ImportantArr = ['has-text-danger', 'has-text-warning', "has-text-success"]
+
+const backgroundColor = ['has-background-danger' ,'has-background-info' ,'has-background-primary'];
+
+
+const MonthArray = ['Января', 'Февраля', 'Марта', 'Апреля', 'Мая', 'Июня','Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря']
+
+
+export { motiv_arr, ImportantArr, backgroundColor, MonthArray };
+
+
+
+
+
+
+
